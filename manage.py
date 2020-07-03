987e0b170d2c818f474ffab56e1cd689590a38ac
Matt Robey <mattrobey@gmail.com>
2020-07-02 20:46:58 -0500

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ import os
 import sys
 
 if __name__ == '__main__':
+    if os.environ.get('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.production')
+else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'azuresite.settings')
     try:
         from django.core.management import execute_from_command_line
